2d6236e1d49c3ad2c7169b4b2b1d64fa0813b844
Everton Afonso <54124207+Everton-Afonso@users.noreply.github.com>
2020-09-22 10:48:10 -0300

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div id="cabe" className="jumbotron">
+      <h1 className="display-4">Sistemas de Informação</h1>
+      <p className="lead">Busca por usuários do GitHub</p>
+    </div>
+  );
+}
+
+export default Header;
